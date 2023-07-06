63c413672aa2c9c832a5c6df7dae4e6df87b5055
ArjenR <arjen@runsink.nl>
2023-07-06 21:38:13 +0200

    Update main.py
    FIx syntaxt and improve logic/fix bad code.

--- he853api/source/main.py
+++ he853api/source/main.py
@@ -9,41 +9,42 @@ app = FastAPI()
 @app.get("/status")
 def get_status():
     status = {}
-    status.update({exitcode: "9"})
-    status.update({result: "FAIL"})
+    status.update({"exitcode": "9"})
+    status.update({"result": "FAIL"})
     result = subprocess.run(["he853-static"], capture_output=True, text=True)
     print(result.stdout)
     status.update({"verbose_result": result.stdout })
     status.update({"exitcode": result.returncode })
     if (result.returncode == "0"):
-        status.update({result: "OK"})
+        status.update({"result": "OK"})
     return status
 
 @app.get("/help")
-def get_status():
+def get_help():
     status = {}
     result = subprocess.run(["he853-static", "--help"], capture_output=True, text=True)
     print(result.stdout)
     status.update({"verbose_result": result.stdout })
     status.update({"exitcode": result.returncode })
     if (result.returncode == "0"):
-        status.update({result: "OK"})
+        status.update({"result": "OK"})
     return status
 
 @app.get("/switch")
 def action(action: str = Query(..., max_length=3, regex="^(on|off|\d{,3)$" ), address: str = Query(..., max_length=4, regex="^\d{4}$"),  protocol: str = Query("A", max_length=1, regex="^[AUEKL]$")):
     
     status = {}
-    status.update({exitcode: "9"})
-    status.update({result: "FAIL"})
-
-    onoff = "0"
+    status.update({"exitcode": "9"})
+    status.update({"result": "FAIL"})
+    onoff = ""
     if (action == "on"):
         onoff = "1"
-    else if (action >= 0 and action <= 255):
+    elif (action == "off"):
+        onoff = "0"
+    elif (action >= 0 and action <= 255):
         onoff = action
     else:
-        status.update({verbose_result: "Action not allowed"})
+        status.update({"verbose_result": "Action not allowed"})
         return status
 
     result = subprocess.run(["he853-static", address, onoff, protocol], capture_output=True, text=True)
@@ -51,9 +52,5 @@ def action(action: str = Query(..., max_length=3, regex="^(on|off|\d{,3)$" ), ad
     status.update({"verbose_result": result.stdout })
     status.update({"exitcode": result.returncode })
     if (result.returncode == "0"):
-        status.update({result: "OK"})
+        status.update({"result": "OK"})
     return status
-
-
-
-    
